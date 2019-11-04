59148bbcf37672f60517337b7667b414a0143cb2
szocs_000 <szocszoltan814@gmail.com>
2019-11-04 09:12:37 +0200

    Proba

--- app/src/main/java/com/example/szoftverprojekt/RegisterActivity.java
+++ app/src/main/java/com/example/szoftverprojekt/RegisterActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
 
     Button btnLogin;
